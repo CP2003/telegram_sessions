a11a6369cd2abfb2f211566b6a83ed62b2c4e0c6
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-12-11 00:56:58 +0530

    Update for-telethon-session.py

--- telegram/for-telethon-session.py
+++ telegram/for-telethon-session.py
@@ -1,13 +1,38 @@
 try:
-    from telethon.sync import TelegramClient
-    from telethon.sessions import StringSession
+    from telethon import TelegramClient
+    from telethon.sessions import StringSession 
+    import asyncio
 except Exception as e:
     print(e)
     print('\nInstall pyrogram: pip install Telethon')
     exit(1)
+    
 
 print('Loading telethon session generator...')
 API_KEY = int(input('Enter API KEY: '))
 API_HASH = input('Enter API HASH: ')
-with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
-    print(client.session.save())
+
+
+async def starting_generator():
+    client = TelegramClient(StringSession(), API_KEY, API_HASH)
+    await client.start()
+    async with client:
+        session = client.session.save()
+        message = f"Here is Your session string \n\n ```{session}```"
+        await client.send_message('me', message)
+        print("Done , Session string has been sent to your saved message. Now you can Close this")
+        
+
+async def main():
+    try:
+        await starting_generator()
+    except:
+        pass
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    try:
+        loop.create_task(main())
+        loop.run_forever()
+    finally:
+        loop.close()
