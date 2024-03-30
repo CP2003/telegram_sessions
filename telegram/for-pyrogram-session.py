35e88a254aa1966f7008b13d0483d40bbbc45039
P A M O D <59545375+CP2003@users.noreply.github.com>
2024-03-30 16:08:44 +0530

    Update for-pyrogram-session.py

--- telegram/for-pyrogram-session.py
+++ telegram/for-pyrogram-session.py
@@ -1,12 +1,17 @@
+import os
+import sys
+import asyncio
 try:
     from pyrogram import Client
-except Exception as e:
-    print(e)
-    print('\nInstall pyrogram: pip3 install pyrogram')
-    exit(1)
+except:
+    print("Intalling telethon")
+    os.system("pip install Telethon")
+    os.system('cls') if os.name == 'nt' else os.system('clear')
+    from pyrogram import Client
 
-print('Required pyrogram V2 or greater.')
 API_KEY = int(input('Enter API KEY: '))
 API_HASH = input('Enter API HASH: ')
 with Client(name='USS', api_id=API_KEY, api_hash=API_HASH, in_memory=True) as app:
+    os.system('cls') if os.name == 'nt' else os.system('clear')
+    print("Here is your session")
     print(app.export_session_string())
